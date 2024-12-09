4702ba1eea7d0a1e8a65c1f3f0bb64ade1c05095
Temenuzhka Vuchova <35531784+tvuchova@users.noreply.github.com>
2024-12-09 15:51:14 +0200

    add spring store and params

--- 01-spring-boot-overview/di-ioc/src/main/java/com/elsys/demo/controller/ParamsController.java
+++ 01-spring-boot-overview/di-ioc/src/main/java/com/elsys/demo/controller/ParamsController.java
@@ -1,6 +1,7 @@
 package com.elsys.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,5 +19,8 @@ public class ParamsController {
         return "ID: " + fooRequest.getId() + " Name: " + fooRequest.getName();
     }
 
-
+    @PostMapping("/api/foos/{id}")
+    public String addFoo(@PathVariable String id, @RequestBody FooRequest fooRequest) {
+        return "Path ID: " + id + ", Body ID: " + fooRequest.getId() + ", Name: " + fooRequest.getName();
+    }
 }
