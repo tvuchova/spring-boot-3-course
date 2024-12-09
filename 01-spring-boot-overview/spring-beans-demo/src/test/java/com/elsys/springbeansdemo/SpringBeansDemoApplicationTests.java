393ba4129a7bbb672c5152bad9056e5fcf7f372b
Temenuzhka Vuchova <35531784+tvuchova@users.noreply.github.com>
2024-12-09 15:29:26 +0200

    add beans

---
+++ 01-spring-boot-overview/spring-beans-demo/src/test/java/com/elsys/springbeansdemo/SpringBeansDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elsys.springbeansdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBeansDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
