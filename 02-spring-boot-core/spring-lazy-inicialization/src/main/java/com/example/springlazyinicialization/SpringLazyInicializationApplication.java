aac0742019adcac2f7e32153251c3987906f3b62
Temenuzhka Vuchova <35531784+tvuchova@users.noreply.github.com>
2024-12-18 08:27:44 +0200

    added lazy inicialization project

---
+++ 02-spring-boot-core/spring-lazy-inicialization/src/main/java/com/example/springlazyinicialization/SpringLazyInicializationApplication.java
@@ -0,0 +1,13 @@
+package com.example.springlazyinicialization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLazyInicializationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLazyInicializationApplication.class, args);
+    }
+
+}
