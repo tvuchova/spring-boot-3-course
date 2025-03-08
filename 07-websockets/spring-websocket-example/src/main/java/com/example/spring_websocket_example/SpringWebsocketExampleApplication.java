8426d2a05d1afed74abb2047f71abe876c22ec89
Temenuzhka Vuchova <35531784+tvuchova@users.noreply.github.com>
2025-03-08 17:53:18 +0100

    add web sockets

---
+++ 07-websockets/spring-websocket-example/src/main/java/com/example/spring_websocket_example/SpringWebsocketExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_websocket_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebsocketExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebsocketExampleApplication.class, args);
+	}
+
+}
---
+++ 07-websockets/spring-websocket-example/src/test/java/com/example/spring_websocket_example/SpringWebsocketExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_websocket_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebsocketExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
