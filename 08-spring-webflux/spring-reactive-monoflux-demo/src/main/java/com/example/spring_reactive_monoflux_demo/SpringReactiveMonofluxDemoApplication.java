46661d042d08eda85ab8d8a35369d7e486c7887c
Temenuzhka Vuchova <35531784+tvuchova@users.noreply.github.com>
2025-03-15 18:02:37 +0200

    add flux and mono

---
+++ 08-spring-webflux/spring-reactive-monoflux-demo/src/main/java/com/example/spring_reactive_monoflux_demo/SpringReactiveMonofluxDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_reactive_monoflux_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveMonofluxDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveMonofluxDemoApplication.class, args);
+	}
+
+}
