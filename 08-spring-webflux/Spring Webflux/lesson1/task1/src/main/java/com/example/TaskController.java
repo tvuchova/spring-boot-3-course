9cef62374af51b366fed683c9cd6e3f201a73c8a
A1B1E68 <temenuzhka.vuchova@rewe-digital.com>
2025-03-20 18:19:03 +0200

    add webflux learning

--- 08-spring-webflux/Spring Webflux/lesson1/task1/src/main/java/com/example/TaskController.java
+++ 08-spring-webflux/Spring Webflux/lesson1/task1/src/main/java/com/example/TaskController.java
@@ -11,6 +11,5 @@ public class TaskController
     // TODO: Implement this method to return a Flux<String>
     public Flux<String> getMessages() {
         return null; // Replace with actual implementation
-
     }
 }
