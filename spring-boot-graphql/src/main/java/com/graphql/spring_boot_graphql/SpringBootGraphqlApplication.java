39c001fa84c2f8bde67ab6e3de60d343498324db
A1B1E68 <temenuzhka.vuchova@rewe-digital.com>
2025-04-13 00:17:14 +0300

    add

---
+++ spring-boot-graphql/src/main/java/com/graphql/spring_boot_graphql/SpringBootGraphqlApplication.java
@@ -0,0 +1,14 @@
+package com.graphql.spring_boot_graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*https://graphql.org/learn/schema/*/
+@SpringBootApplication
+public class SpringBootGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGraphqlApplication.class, args);
+	}
+
+}
--- spring-boot-hateas/src/main/java/com/example/spring_boot_hateas/resthateoas/LogExecutionTime.java
+++ spring-boot-hateas/src/main/java/com/example/spring_boot_hateas/resthateoas/LogExecutionTime.java
@@ -1,4 +1,12 @@
 package com.example.spring_boot_hateas.resthateoas;
 
-public class LogExecutionTime {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface LogExecutionTime {
+
 }
